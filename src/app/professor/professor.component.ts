726fc5a32ffa9060ace98b2127d0ba214283c62f
JessMotta <jessicalimamotta@gmail.com>
2022-06-21 17:14:40 -0300

    add bootstrap and add teacher's table

--- src/app/professor/professor.component.ts
+++ src/app/professor/professor.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Professor } from './professor';
+
+const LISTA_PROFESSORES: Professor[]= [
+  { nome: 'Magali', email: 'magali@gmail.com', idade: 35, disciplina: 'História' },
+  { nome: 'Márcia', email: 'marcia@gmail.com', idade: 20, disciplina: 'Geografia' },
+  { nome: 'Daniel', email: 'daniel@gmail.com', idade: 41, disciplina: 'Ciências' },
+  { nome: 'Gil', email: 'gil@gmail.com', idade: 50, disciplina: 'Informática' }
+]
 
 @Component({
   selector: 'spa-professor',
   templateUrl: './professor.component.html',
   styleUrls: ['./professor.component.css']
 })
-export class ProfessorComponent implements OnInit {
 
+export class ProfessorComponent implements OnInit {
+  
+  exibeTabela: boolean = false;
+  
   constructor() { }
 
+  displayColumns: string[] = ['nome', 'email', 'idade', 'disciplina']
+  dataSource = LISTA_PROFESSORES
+
   ngOnInit(): void {
+  }
+
+  exibirTabela(){
+    this.exibeTabela = true
+  }
+
+  esconderTabela(){
+    this.exibeTabela = false
   }
 
 }
---
+++ src/app/professor/professor.ts
@@ -0,0 +1,14 @@
+export class Professor{
+    nome: string;
+    email: string;
+    idade: number;
+    disciplina: string;
+
+    constructor(){
+        this.nome = "";
+        this.email = "";
+        this.idade = 0;
+        this.disciplina = "";
+
+    }
+}
